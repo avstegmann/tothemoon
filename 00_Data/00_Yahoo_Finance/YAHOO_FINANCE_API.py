3b0bbddcc50cdd453363de4aca2e2dc2f9589181
eXpresso94 <74989859+eXpresso94@users.noreply.github.com>
2021-04-03 12:26:03 +0200

    Update_Final
    Yahoo_API ist fertig :)

--- 00_Data/00_Yahoo_Finance/YAHOO_FINANCE_API.py
+++ 00_Data/00_Yahoo_Finance/YAHOO_FINANCE_API.py
@@ -4,7 +4,7 @@ import pandas as pd
 from yfinance import Ticker
 
 
-tickerStrings = ['AMC', 'GME', 'BB','KOSS','EXPR','ZOM','NOK']
+tickerStrings = ['AMC', 'GME', 'BB','KOSS','EXPR','ZOM','NOK','NAKD','PLTR','JAGX','SNDL','BBBY','TSM','CCIV','TSLA','FORD','ASO','MVIS','AMD','GM','WIRE','M','CURLF','NFLX','NCTY','TIRX','CTRM','RLX','EZGO','SRNE','CVM','LGND','FIZZ']
 df_list = list()
 for ticker in tickerStrings:
     data = yf.download(ticker, group_by="Ticker", period='max')
@@ -16,3 +16,25 @@ df = pd.concat(df_list)
 
 # save to csv
 df.to_csv('Yahoo_Finance_Data.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
