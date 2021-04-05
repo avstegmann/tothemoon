6df3a030e54e6d2ffaadd21339da58ac58d6d16c
eXpresso94 <74989859+eXpresso94@users.noreply.github.com>
2021-04-05 18:36:01 +0200

    TwitterCode Update

--- 00_Data/02_Twitter/Twitter_Search.py
+++ 00_Data/02_Twitter/Twitter_Search.py
@@ -1,53 +1,20 @@
-import twint
-import csv
-
-c = twint.Config()
-
-c.Search = "GME OR Gamestop"
-c.Since = "2019-01-01 00:00:00"
-c.Output = "Twitter_1.csv"
-c.Limit = 10
-
-twint.run.Search(c)
-
-print('Inder machen geile Codes')
+# Dieser Code sucht nach den Stichworten in Search Terms
 
 import twint
-import pandas
-import datetime
+import os
 
+search_terms = ['AMC', 'BB','KOSS','EXPR','ZOM','NOK','NAKD','PLTR','JAGX','SNDL','BBBY','TSM','CCIV','TSLA','FORD','ASO','MVIS','AMD','GM','WIRE','M','CURLF','NFLX','NCTY','TIRX','CTRM','RLX','EZGO','SRNE','CVM','LGND','FIZZ']
 
+def jobone():
+    for i in range(len(search_terms)):
+        c = twint.Config()
+        c.Search = search_terms[i]
+        c.Lang = "en"
+        c.Since = "2019-01-01 00:00:00"
+        # c.Limit = 100
+        c.Store_csv = True
+        c.Output = os.path.join('../02_Twitter/{}.csv'.format(search_terms[i]))
 
+        twint.run.Search(c)
 
-
-#all_stocks = ['AMC', 'GME', 'BB','KOSS','EXPR','ZOM','NOK','NAKD','PLTR','JAGX','SNDL','BBBY','TSM','CCIV','TSLA','FORD','ASO','MVIS','AMD','GM','WIRE','M','CURLF','NFLX','NCTY','TIRX','CTRM','RLX','EZGO','SRNE','CVM','LGND','FIZZ']
-#def scrape_by_stock(keywords, since, outfile):
-#	unique_stocks = set(all_stocks) #To get unique cities of country
-#	stocks = sorted(unique_stocks) #Sort & convert datatype to list
-#	for stock in stocks:
-#		print(stock)
- #       c.Search = keywords #search keyword
-  #      c.Since = since
-   #     c.Store_csv = True
-   	#    c.Output = "./" + outfile
-     #   c.Near = stock
-      #  c.Hide_output = True
-       # c.Count = True
-       # c.Stats = True
-        #c.Resume = 'resume.txt'
-        #twint.run.Search(c)
-
-#scrape_by_stock(all_stocks, '2021-04-1 15:55:00', 'Test.csv')
-
-list_ = ['AMC', 'BB','KOSS','EXPR','ZOM','NOK','NAKD','PLTR','JAGX','SNDL','BBBY','TSM','CCIV','TSLA','FORD','ASO','MVIS','AMD','GM','WIRE','M','CURLF','NFLX','NCTY','TIRX','CTRM','RLX','EZGO','SRNE','CVM','LGND','FIZZ']
-
-
-for i in list_:
-    print(i)
-    c = twint.Config()
-    c.Search = i
-    c.Pandas = True
-    c.Since = "2021-03-01"
-    c.Until = "2021-03-04"
-    c.Custom_csv = ["id", "user_id", "username", "date", "tweet"]
-    c.Output = "test.csv"
+jobone()
