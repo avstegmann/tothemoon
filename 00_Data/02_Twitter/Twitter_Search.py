83354f4916869fd63a14f23986e76dc039d318db
eXpresso94 <74989859+eXpresso94@users.noreply.github.com>
2021-04-25 20:11:16 +0200

    Update

--- 00_Data/02_Twitter/Twitter_Search.py
+++ 00_Data/02_Twitter/Twitter_Search.py
@@ -1,21 +1,21 @@
-# Dieser Code sucht nach den Stichworten in Search Terms - Datenmenge wird vielleicht zu gross 'AMC','GME','TSLA',
+# Dieser Code sucht nach den Stichworten in Search Terms 
 
 
 import twint
 import os
 
-search_terms = ['BB','KOSS','EXPR','ZOM','NOK','NAKD','PLTR','JAGX','SNDL','BBBY','TSM','CCIV','FORD','ASO','MVIS','AMD','GM','WIRE','M','CURLF','NFLX','NCTY','TIRX','CTRM','RLX','EZGO','SRNE','CVM','LGND','FIZZ']
+search_terms = ['AMC', 'GME', 'BB','KOSS','EXPR','ZOM','NOK','NAKD','PLTR','JAGX','SNDL','BBBY','TSM','CCIV','TSLA','FORD','ASO','MVIS','AMD','GM','WIRE','CURLF','NFLX','NCTY','TIRX','CTRM','RLX','EZGO','SRNE','CVM','LGND','FIZZ']
 
-def jobone():
+def fun_mit_twitter():
     for i in range(len(search_terms)):
         c = twint.Config()
         c.Search = search_terms[i]
         c.Lang = "en"
-        c.Since = "2019-01-01 00:00:00"
-        # c.Limit = 100 - viellecht müesse mer paar ihschränkige mache
+        c.Since = "2016-01-01 00:00:00"
+        # c.Limit = 100 - Einschränkungen werden nötig
         c.Store_csv = True
         c.Output = os.path.join('../02_Twitter/{}.csv'.format(search_terms[i]))
 
         twint.run.Search(c)
 
-jobone()
+fun_mit_twitter()
